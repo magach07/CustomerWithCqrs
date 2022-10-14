155afb9c56c45f33f67acafbdb2ca36493749917
Jonathan Magacho <jonathanmagacho@gmail.com>
2022-10-14 17:28:28 -0300

    adicionando try catch para o tratamento de erro

--- Domain/Handlers/CreateCustomerHandler.cs
+++ Domain/Handlers/CreateCustomerHandler.cs
@@ -2,7 +2,6 @@ using System.Net;
 using System.Net.Mail;
 using CustomerWithCqrs.Domain.Commands.Requests;
 using CustomerWithCqrs.Domain.Commands.Responses;
-using Microsoft.AspNetCore.Mvc;
 
 namespace CustomerWithCqrs.Domain.Commands.Handlers
 {
@@ -16,7 +15,8 @@ namespace CustomerWithCqrs.Domain.Commands.Handlers
 
             if (exists == false)
             {
-                CreateCustomerResponse addCustomer = new CreateCustomerResponse{
+                CreateCustomerResponse addCustomer = new CreateCustomerResponse
+                {
                     Id = Guid.NewGuid(),
                     Name = request.Name,
                     Email = request.Email,
@@ -25,21 +25,36 @@ namespace CustomerWithCqrs.Domain.Commands.Handlers
 
                 responseCustomer.Add(addCustomer);
 
-                MailMessage email = new MailMessage();
-                email.From = new MailAddress("jonathanmagacho@gmail.com");
-                email.To.Add(request.Email);
-                email.Subject = "Customer C#";
-                email.IsBodyHtml = true;
-                email.Body = "<p>Olá " + request.Name + ", você foi adicionado a lista de Clientes da aplicação C# desenvolvida por Jonathan Magacho. <p>";
+                try
+                {
+                    MailMessage email = new MailMessage();
+                    email.From = new MailAddress("jonathanmagacho@gmail.com");
+                    email.To.Add(request.Email);
+                    email.Subject = "Customer C#";
+                    email.IsBodyHtml = true;
+                    email.Body = "<p>Olá " + request.Name + ", você foi adicionado a lista de Clientes da aplicação C# desenvolvida por Jonathan Magacho.<p>";
 
-                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
-                smtp.UseDefaultCredentials = false;
+                    SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                    smtp.UseDefaultCredentials = false;
 
-                //A senha é gerada unicamente para o processo dentro das configurações da conta google
-                smtp.Credentials = new NetworkCredential("jonathanmagacho@gmail.com", "bbczgipyemwjecuk");
-                smtp.EnableSsl = true;
+                    //A senha é gerada unicamente para o processo dentro das configurações da conta Google.
+                    smtp.Credentials = new NetworkCredential("jonathanmagacho@gmail.com", "bbczgipyemwjecuk");
+                    smtp.EnableSsl = true;
 
-                smtp.Send(email);
+                    smtp.Send(email);
+                }
+                catch (System.Exception e)
+                {
+                    CreateCustomerResponse errorEmail = new CreateCustomerResponse
+                    {
+                        Id = Guid.Empty,
+                        Name = "Erro ao enviar email.",
+                        Email = e.Message,
+                        InsertDate = DateTime.Now
+                    };
+
+                    return errorEmail;
+                }
 
                 return addCustomer;
             }
